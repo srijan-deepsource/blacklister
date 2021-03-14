d0d0dda17ed9e64376e5a9269ce28382cd7b4fab
Srijan Saurav <sauravsrijan@gmail.com>
2021-03-14 15:10:32 +0530

    Add logic

--- analyze.py
+++ analyze.py
@@ -1,3 +1,5 @@
+import os
+
 from helper import (
     make_issue,
     prepare_result,
@@ -5,3 +7,41 @@ from helper import (
 )
 
 # Add your analyzer logic below
+def analyze():
+    blacklisted_names = ("deepsource", "Deepsource", "deepSource")
+
+    codepath = os.environ.get("CODE_PATH", "/code")
+
+    issues = []
+    lines = []
+
+    filepath = os.path.join(codepath, "README.md")
+    try:
+
+        with open(
+            filepath
+        ) as fp:
+            lines = fp.readlines()
+    except:
+        pass
+
+    for idx, line in enumerate(lines):
+        lno = idx + 1
+        for bad_ds in blacklisted_names:
+            if bad_ds in line:
+                issues.append(
+                    make_issue(
+                        "BAD-DS",
+                        f"Blacklisted word: {bad_ds}",
+                        filepath,
+                        lno,
+                        0
+                    )
+                )
+
+    results = prepare_result(issues)
+    publish_results(results)
+
+
+if __name__ == "__main__":
+    analyze()
